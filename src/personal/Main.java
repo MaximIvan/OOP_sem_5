5fb8f66bc4a399e4ad02abe8576a608d8d00f5c6
Maksim <homeice@mail.ru>
2023-03-04 18:31:57 +0300

    remove file users.txt

--- src/personal/Main.java
+++ src/personal/Main.java
@@ -10,7 +10,7 @@ import personal.views.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
-        FileOperation fileOperation = new FileOperationImpl("users.txt");
+        FileOperation fileOperation = new FileOperationImpl("C:\\Users\\Максим\\OneDrive\\Рабочий стол\\OOP_education\\Seminars\\Seminar_5\\src\\users.txt");
         Repository repository = new RepositoryFile(fileOperation);
         UserController controller = new UserController(repository, new Validation());
         ViewUser view = new ViewUser(controller);
